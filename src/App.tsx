8d3e7f6e9d6853dfa3c4bc9268c88c9f14086bc1
Rhuan Santos <50742592+rhuangabrielsantos@users.noreply.github.com>
2021-08-15 15:53:45 -0300

    :tada: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center bg-nosferatu w-screen h-screen">
+      <h1 className="text-cullen text-3xl font-roboto font-light">
+        Boilerplate tailwind with react.js
+      </h1>
+    </div>
+  );
+}
+
+export default App;
